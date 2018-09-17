a3eff291f2ee22538dc8ed444756c21cfade60bd
jhonasr <jhonalex0525@gmail.com>
2018-09-16 22:46:22 -0500

    second commit

--- solution.rb
+++ solution.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 
 get '/' do
 	unless params[:nombre]
-		"Hola desconocido!"
+		<<-HTML
+		<h1>"Hola desconocido!"</h1>
+		HTML
 	else 
 		<<-HTML
 		<h1>Hola #{params[:nombre]}!</h1>
